0377ae67c557452b3985c9ebcffb936e7df3e3b2
Charlie Albright <albrightc25@gmail.com>
2016-02-04 13:57:38 -0600

    Trying simple route response.

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+var router = express.Router()
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+router.get('/', function(request, response) {
+  response.json({message: "It works!"});
 });
+
+app.use('/api', router);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
