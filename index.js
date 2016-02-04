c3c5d529c5328a6d5efbbe7cf2468a7036245e19
Spencer Kaiser <skaiser@me.com>
2016-02-04 16:35:35 -0600

    Adding setResponse functionality, regex for help command

--- index.js
+++ index.js
@@ -2,29 +2,56 @@ var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
 var router = express.Router()
 
-router.post('/', function(request, response) {
-	response.json({
-		"response_type": "ephemeral",
-		"text": "Development of SlotBot is currently underway!",
-		"attachments": [
-			{
-				"text": request.body.text
-			}
-		]
-	  });
+router.post('/', function (request, response) {
+
+    var helpRE = /help/i;
+
+    if (request.body.text.match(helpRE)) {
+        setResponse(request, response, "ephemeral", "Here is teh HALP");
+    } else {
+        setResponse(request, response, "ephemeral", "Development of SlotBot is currently underway!", request.body.text);
+    }
+
+    //	response.json({
+    //		"response_type": "ephemeral",
+    //		"text": "Development of SlotBot is currently underway!",
+    //		"attachments": [
+    //			{
+    //				"text": request.body.text
+    //			}
+    //		]
+    //	  });
 });
+
+
+function setResponse(request, response, type, text, attachmentText) {
+    if (!type) {
+        type = "ephemeral";
+    }
+
+    response.json({
+        "response_type": type,
+        "text": text,
+        "attachments": [
+            {
+                "text": attachmentText
+                }
+            ]
+    });
+}
 
 app.use('/api', router);
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
