5fa8e307a7d229a5950724b8045d08739b5989d9
Spencer Kaiser <skaiser@me.com>
2016-02-05 14:40:50 -0600

    Switching create party response to a message

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ router.post('/', function (request, response) {
                         var time = "5:00PM";
                         var date = "Today";
 
-                        setResponse(request, response, "in_channel", "@" + gamertag + " has created a new party (" + time + " - " + date + ")! To join, type `/slotbot join party " + partyID + "`");
+                        sendMessage("@" + gamertag + " has created a new party (" + time + " - " + date + ")! To join, type `/slotbot join party " + partyID + "`", request.body.response_url);
                     } else {
                         setResponse(request, response, "ephemeral", "It looks like you didn't use a valid command or you forgot to include required parameters. Try using `/slotbot help`! :kissing_heart:");
                     }
