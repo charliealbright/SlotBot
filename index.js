998a8accb3b57dad4876f27160e410270f02440c
Charlie Albright <albrightc25@gmail.com>
2016-02-05 21:54:31 -0600

    whoops

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var requests = require('request');
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
 
 // GLOBAL VARS
 var games, users, helpJSON;
@@ -19,6 +20,12 @@ readGames();
 readUsers();
 readHelpJSON();
 connectToDB();
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
 
